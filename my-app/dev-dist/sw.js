74b4302437e3a27122a123a139cd0ecc1b226760
Surovets Elizaveta <surovets.liz@gmail.com>
2025-12-12 05:36:47 +0300

    https backend

--- my-app/dev-dist/sw.js
+++ my-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.o85ev5i3h9g"
+    "revision": "0.qr8is68p5ro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
