1a74e9e2285d64667bdc5b05a1a1f9791150fe25
Surovets Elizaveta <surovets.liz@gmail.com>
2025-12-18 22:21:08 +0300

    backend fix

--- my-app/dev-dist/sw.js
+++ my-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.k37ac0phhlg"
+    "revision": "0.li5j33m6pa8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- my-app/target_config.ts
+++ my-app/target_config.ts
@@ -1,7 +1,7 @@
 const target_tauri = true
 
-export const api_proxy_addr = "http://172.20.32.1:8080"
-export const img_proxy_addr = "http://172.20.32.1:9000"
+export const api_proxy_addr = "https://10.65.104.65:8080"
+export const img_proxy_addr = "http://10.65.104.65:9000"
 export const dest_api = (target_tauri) ? `${api_proxy_addr}/api` : "/api"
 export const dest_img = (target_tauri) ? img_proxy_addr : "/img-proxy"
 export const dest_root = (target_tauri) ? "" : "/"
