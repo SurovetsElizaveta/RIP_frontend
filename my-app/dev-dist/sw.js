c72609f32438e66713a914b117328e2827df27c7
Surovets Elizaveta <surovets.liz@gmail.com>
2025-12-26 00:39:51 +0300

    profile page fix

--- my-app/dev-dist/sw.js
+++ my-app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.sdmkas2tdng"
+    "revision": "0.1o9oqfm798o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
