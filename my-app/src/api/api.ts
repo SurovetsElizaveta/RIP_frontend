0a5def44cf5b0e6953fa349e4fa0662b5a6a801a
Surovets Elizaveta <surovets.liz@gmail.com>
2025-12-26 12:50:04 +0300

    Ip fix

--- my-app/src/api/api.ts
+++ my-app/src/api/api.ts
@@ -1,6 +1,6 @@
 import type { Route } from '../types/types';
 
-const YOUR_LOCAL_IP = '192.168.0.78';
+const YOUR_LOCAL_IP = '10.92.235.65';
 const API_BASE = `https://${YOUR_LOCAL_IP}:8080/api`;
 
 export const ROUTES_MOCK: Route[] = [
